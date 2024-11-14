21760547e7cd0b6c89a43e6d979969dfb29171a6
CodeRain49 <coderain49@gmail.com>
2024-11-14 16:49:12 +0700

    upload

---
+++ Program.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateGame2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int correct_answer;
+            int total_score = 0;
+            int factor1; //random number of the first factor
+            int factor2; //random number of the second factor
+            int playerAnswer;
+
+            // -- Intro -- 
+            ChangeColor("Yellow");
+            Console.WriteLine("Welcome to the Math game!");
+            ChangeColor("Red");
+            Console.WriteLine("-------------------------");
+            ChangeColor(default);
+
+            // -- player name info --
+            Console.WriteLine("Type your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Player Name : ");
+            ChangeColor("Cyan");
+            Console.WriteLine(name);
+            ChangeColor(default);
+            Console.WriteLine();
+
+            // -- Game mode/difficulty selection --
+            Console.WriteLine("----Choose the Difficulty----");
+            Console.WriteLine();
+            Console.WriteLine("Type game Mode/Difficult: '1' for EASY or '2' for HARD mode");
+            int gameMode = Convert.ToInt32(Console.ReadLine());
+            switch (gameMode){
+                case 1:
+                    ChangeColor("Green");
+                    Console.WriteLine("---- Easy mode selected! ----");
+                    break;
+
+                case 2:
+                    ChangeColor("Red");
+                    Console.WriteLine("---- Hard mode selected!!! ----");
+                    break;
+
+                default:
+                    ChangeColor("Green");
+                    Console.WriteLine("---- Easy mode (Default) ----");
+                    break;
+            }
+            ChangeColor(default);
+            Console.WriteLine();
+
+            // -- Player choose the Operator --
+            Console.WriteLine("---- Should the operator (type the Number) ----");
+            Console.WriteLine("'1' = (Plus +), '2' = (Minus -), '3' = (Mutiply *)");
+            int selectedOperator = Convert.ToInt32(Console.ReadLine());
+            string operatorSelectString = ""; //Convert to string variable type
+            ChangeColor("Yellow");
+            switch (selectedOperator){
+                case 1:
+                    Console.WriteLine("---- Addition Game Mode (+) ----");
+                    operatorSelectString = "+";
+                    break;
+
+                case 2:
+                    Console.WriteLine("---- Subtraction Game Mode (-) ----");
+                    operatorSelectString = "-";
+                    break;
+
+                case 3:
+                    Console.WriteLine("---- Mutiplication Game Mode (*) ----");
+                    operatorSelectString = "*";
+                    break;
+
+                //case 4:
+                //    Console.WriteLine("---- Division Game Mode (/) ----");
+                //    operatorSelectString = "/";
+                //    break;
+
+                default:
+                    Console.WriteLine("---- Addition Game Mode (+) ----");
+                    operatorSelectString = "+";
+                    break;
+            }
+
+            // --- Main code after all have selected ---
+            Calculation();
+
+            // --- Score Result ---
+            Console.WriteLine();
+            ChangeColor("Cyan");
+            Console.WriteLine("Player name: " + name);
+            Console.WriteLine("GET TOTAL SCORE OF: " + total_score);
+
+            // [ALL Function]
+            // Execute all the quiz code with Easy mode selected
+            void Calculation()
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    ChangeColor("Yellow");
+                    if (gameMode == 2){
+                        RandomNumberHardMode();
+                    }
+                    else{
+                        RandomNumberEasyMode();
+                    }
+
+                    switch (selectedOperator)
+                    {
+                        case 1:
+                            correct_answer = factor1 + factor2;
+                            break;
+                        case 2:
+                            correct_answer = factor1 - factor2;
+                            break;
+                        case 3:
+                            correct_answer = factor1 * factor2;
+                            break;
+                        //case 4:
+                        //    correct_answer = factor1 / factor2;
+                        //    break;
+                        default:
+                            Console.WriteLine("//Invalid operator. Defaulting to Addition.//");
+                            correct_answer = factor1 + factor2;
+                            break;
+                    }
+
+                    ChangeColor("default");
+                    playerAnswer = Convert.ToInt32(Console.ReadLine()); // Get Player answer
+
+                    if (playerAnswer == correct_answer)
+                    {
+                        PlayerCorrect();
+                    }
+                    else if (playerAnswer != correct_answer)
+                    {
+                        PlayerGetWrongAnswer();
+                    }
+                }
+            }
+
+            // Player got the correct answer, +1 point to the total score
+            void PlayerCorrect() 
+            {
+                ChangeColor("Green");
+                Console.WriteLine("Correct!!");
+                Console.WriteLine(); //Space
+                ChangeColor("default");
+                total_score++;
+            }
+
+            // Player got wrong answer, no point
+            void PlayerGetWrongAnswer()
+            {
+                ChangeColor("Red");
+                Console.WriteLine("Wrong! the correct answer is -> " + correct_answer);
+                Console.WriteLine(); //Space
+                ChangeColor("default");
+            }
+
+            // Random number for easy mode >100
+            void RandomNumberEasyMode()
+            {
+                Random rnd = new Random();
+                factor1 = rnd.Next(1, 99);
+                factor2 = rnd.Next(1, 99);
+                Console.WriteLine(factor1 + " " + operatorSelectString + " " + factor2);
+            }
+
+            // Random number for hard mode >1000
+            void RandomNumberHardMode()
+            {
+                Random rnd = new Random();
+                factor1 = rnd.Next(1, 999);
+                factor2 = rnd.Next(1, 999);
+                Console.WriteLine(factor1 + " " + operatorSelectString + " " + factor2);
+            }
+
+            // Change color for aesthetics
+            void ChangeColor(string color){
+                switch (color)
+                {
+                    case "Green":
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        break;
+                    case "Cyan":
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        break;
+                    case "Yellow":
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        break;
+                    case "Red":
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        break;
+                    default:
+                        Console.ForegroundColor = ConsoleColor.White;
+                        break;
+                }
+            }
+
+            Console.ReadKey();
+        } 
+    }
+}
+
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CalculateGame2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("IC Shop And IC Service")]
+[assembly: AssemblyProduct("CalculateGame2")]
+[assembly: AssemblyCopyright("Copyright © IC Shop And IC Service 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5cc1d34c-79c2-4f88-a647-4ba045ee1cc1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
